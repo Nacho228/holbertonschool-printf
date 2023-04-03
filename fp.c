cc1ee2270ae3ab7064da3e026698ae1f3337d2d9
root <Nacho>
2023-04-02 19:26:45 -0700

    everything looks prettier

--- fp.c
+++ fp.c
@@ -3,60 +3,58 @@
 
 int (*fp)(va_list);
 
-fp_t ops[] =
+	fp_t ops[] =
 
-{
+	{	
 
-{'c', pf_c},
-{'s', pf_s},
-{'\0', NULL},
-};
+	{'c', pf_c},
+	{'s', pf_s},
+	{'\0', NULL},
+	};
 
 int pf_c(va_list args)
 {
-int a = va_arg(args, int);
-return (_putchar(a));
+	int a = va_arg(args, int);
+	return (_putchar(a));
 }
 
 int pf_s(va_list args)
 {
-const char *str = va_arg(args, const char *);
-int count = 0;
-while (*str)
-{
-_putchar(*str++);
-count++;
-}
+	const char *str = va_arg(args, const char *);
+	int count = 0;
+	while (*str)
+	{
+		_putchar(*str++);
+		count++;
+	}
 return (count);
 }
 
-<<<<<<< HEAD
-=======
 int pf_d(va_list args)
 {
-int num = va_arg(args, int);
-int count = 0;
+	int num = va_arg(args, int);
+	int count = 0;
 
-if (num < 0)
-{
-putchar('-');
-count++;
-num = -num;
-}
+	if (num < 0)
+	{
+		putchar('-');
+		count++;
+		num = -num;
+	}
 
-if (num / 10)
-{
-count += pf_d(args);
-}
+	if (num / 10)
+	{
+		count += pf_d(args);
+	}
 
-putchar(num % 10 + '0');
-count++;
+	putchar(num % 10 + '0');
+	count++;
 
 return (count);
 }
 
->>>>>>> 900e1be8df9c77678c2d0338298a5be217e8dc08
+
 void init_fp(void)
 {
-fp = &pf_c;
+	fp = &pf_c;
 }
